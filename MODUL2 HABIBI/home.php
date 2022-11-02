b8e77da939b0f0ef38279762da98f309e3bbfa09
Habibiakmal <muhammad.habibi.ha@gmail.com>
2022-11-02 11:44:01 +0700

    modul2

---
+++ MODUL2 HABIBI/home.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container d-flex justify-content-center">
+      <ul class="navbar-nav">
+        <li class=" nav-item">
+          <a class="nav-link" href="home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="booking.php">Booking</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+
+    <br>
+    <h4 align="center">WELCOME TO EAD RENT</h4>
+    <br>
+    <p align="center">Find your best deal, here!</p>
+    <br>
+    <div class="row">
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+          <img src="innova.png" alt="">
+          <div class="card-body">
+            <h5 class="card-title">Toyota Innova</h5>
+            <p class="card-text">Rp. 230.000 / Day</p>
+            <ul class="list-group list-group-flush" style="text-align :center;">
+              <li class="list-group-item text-primary">8 Kursi</li>
+              <li class="list-group-item text-primary">1500 CC</li>
+              <li class="list-group-item text-primary">Manual</li>
+            </ul>
+          </div>
+          <div class="card-footer d-flex justify-content-center">
+            <form action="booking.php" method="post">
+              <button class="btn btn-primary" name="img" type="submit" value="rush.png">Booking Now</button>
+          </form>
+        </div>
+      </div>
+    </div>
+      
+
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+          <img src="brio.webp" class="card-img-top" alt="">
+            <div class="card-body">
+              <h5 class="card-title">Honda Brio</h5>
+              <p class="card-text">Rp. 150.000 / Day</p>
+             <ul class="list-group list-group-flush" style="text-align :center;">
+              <li class="list-group-item text-primary">5 Kursi</li>
+              <li class="list-group-item text-primary">1200 CC</li>
+              <li class="list-group-item text-primary">Manual</li>
+            </ul>
+            </div>
+            <div class="card-footer d-flex justify-content-center">
+              <form action="booking.php" method="post">
+                <button class="btn btn-primary" name="img" type="submit" value="ayla.png">Booking Now</button>
+              </form>
+          </div>
+        </div>
+      </div>
+
+      <div class="col">
+        <div class="card" style="width: 18rem;">
+        <img src="jazz.webp" class="card-img-top" alt="" style="width: 215px;">
+        <div class="card-body">
+          <h5 class="card-title">Honda Jazz</h5>
+          <p class="card-text">Rp. 180.000 / day</p>
+          <ul class="list-group list-group-flush" style="text-align :center;">
+            <li class="list-group-item text-primary">5 Kursi</li>
+            <li class="list-group-item text-primary">1500 CC</li>
+            <li class="list-group-item text-primary">Automatic</li>
+          </ul>
+        </div>
+  
+          <div class="card-footer d-flex justify-content-center">
+            <form action="booking.php" method="post">
+              <button class="btn btn-primary"name="img"value="brio.jpeg">Booking Now</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br>
+
+  </div>
+  <footer align="center" class="bg-light p-2">
+    <p>Habibi_1202204284</p>
+  </footer>
+</body>
+
+</html>
---
+++ MODUL2 HABIBI/mybooking.php
@@ -0,0 +1,156 @@
+<?php
+$credit = "Habibi 1202204284";
+
+$listmobil = [
+    [
+        "tipe" => "Innova",
+        "harga" => 230000,
+        "kursi" => 7,
+        "cc" => 5000,
+        "mesin" => "Manual",
+        "img" => "innova.png"
+    ],
+    [
+        "tipe" => "Brio",
+        "harga" => 150000,
+        "kursi" => 5,
+        "cc" => 1700,
+        "mesin" => "Otomatis",
+        "img" => "brio.png"
+    ],
+    [
+        "tipe" => "Jazz",
+        "harga" => 80000,
+        "kursi" => 5,
+        "cc" => 2000,
+        "mesin" => "Otomatis",
+        "img" => "jazz.webp"
+    ],
+];
+
+$book = $_POST["book"]." ".$_POST["time"];
+$starev = date("d-m-Y H:i", strtotime($book));
+$endev = date("d-m-Y H:i", strtotime($book)+60*60*24*$_POST["hari"]);
+$mobil = $_POST["mobil"];
+$phone = $_POST["phone"];
+
+$service = 0; 
+if (isset($_POST["ser"])){
+    foreach ($_POST["ser"] as $ser ){
+        if ($ser == "Health Protocol"){
+            $service += 700000;
+        }
+        if ($ser == "Full filled"){
+            $service += 450000;
+        }
+        if ($ser == "Driver"){
+            $service += 250000;
+        }
+    }
+}
+else{
+    $service += 0;
+}
+
+?>
+
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+        <title>EAD RENT</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+            <div class="container-fluid">
+            
+                <div class="collapse navbar-collapse justify-content-md-center">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="home.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="booking.php">Booking</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container" style="text-align: center;">
+            <div class="m-2">
+            <p class="m-0" style="font-size:30px">Thank you <?php echo $credit;?> for Reserving</p>
+                <p class="m-0" style="font-size:25px">Please double check your reservation detail</p>
+            </div>
+        </div>
+        
+        <div class="container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Booking Number</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Check-in</th>
+                        <th scope="col">Check-out</th>
+                        <th scope="col">Car Type</th>
+                        <th scope="col">Phone Number</th>
+                        <th scope="col">Service</th>
+                        <th scope="col">Total Price</th>
+                    </tr>
+                </thead>
+                <tbody class="bg-light">
+                    <tr>
+                        <td><?php echo rand (8000000, 10000000);?></td>
+                        <td><?php echo $credit; ?></td>
+                        <td><?php echo $starev?></td>
+                        <td><?php echo $endev?></td>
+                        <td><?php echo $mobil?></td>
+                        <td><?php echo $phone?></td>
+                        <td>
+                            <?php
+                                if (isset($_POST["ser"])){
+                                    foreach ($_POST["ser"] as $ser ){
+                                        echo "<li>$ser</li>";
+                                    }
+                                }
+                                else{
+                                    echo "no service";
+                                }
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                                if ($_POST["mobil"] == $listmobil[0]["tipe"]){
+                                    echo ($_POST["hari"]*$listmobil[0]["harga"])+$service;
+                                }
+                                else if ($_POST["mobil"] == $listmobil[1]["tipe"]){
+                                    echo ($_POST["hari"]*$listmobil[1]["harga"])+$service;
+                                }
+                                else if ($_POST["mobil"] == $listmobil[2]["tipe"]){
+                                    echo ($_POST["hari"]*$listmobil[2]["harga"])+$service;
+                                }
+                                else{
+                                    echo "0";
+                                }
+                            ?>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <footer class="fixed-bottom text-center pt-3 bg-light">
+            <p style="text-align: center; color: lightslategray;">
+                Created by: <?php echo $credit;?>
+            </p>
+        </footer>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    </body>
+</html>
